04d336fa7c25d8584dcffecfd69188e21638a62f
andrew <andrewcarr06@gmail.com>
2019-02-01 16:36:07 -0700

    oops

--- cnp.py
+++ cnp.py
@@ -174,9 +174,9 @@ class ResEncoder(nn.Module):
             x = self.conv1(x)
         x = self.conv2(x)
         x = self.internal_model(x)
-        print(x.shape)
+        print("input: ", x.shape)
         x = x + self.a * torch.exp(torch.mm(self.W, x.view(1000,1)) + self.b) # one neuron
-        print(x.shape)
+        print("output: ", x.shape)
         return x.view(1, 1000)
 
 class MRIDecoder(nn.Module):
